7e11621428048ab30ade368b9fc7750e93327f33
Alex Chase <55210806+achase124@users.noreply.github.com>
2019-09-27 15:01:38 -0400

    script-includes (From inc folder)

---
+++ script-includes.php
@@ -0,0 +1,4 @@
+<!-- Should be pasted in files in order for the menu highlighter to work -->
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="scripts/menu-highlighter.js"></script>
