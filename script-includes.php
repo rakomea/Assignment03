b1b7c0bd9636afd0a5bb16de0dea4cde65d0b859
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-30 12:49:47 -0400

    Delete script-includes.php

--- script-includes.php
+++
@@ -1,4 +0,0 @@
-<!-- Should be pasted in files in order for the menu highlighter to work -->
-
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="scripts/menu-highlighter.js"></script>
